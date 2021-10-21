4da45b56a6672030e7ff2d20ad620e258a6f0950
Pound <pond_nattapoom@hotmail.com>
2021-10-21 15:32:32 +0700

    structure-pattern

---
+++ Stucture-pattern/SoftArchDesign/src/edu/parinya/softarchdesign/structural/HealthcareWorkerTeam.java
@@ -0,0 +1,27 @@
+package edu.parinya.softarchdesign.structural;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HealthcareWorkerTeam implements HealthcareServiceable{
+ private Set<HealthcareServiceable> members = new HashSet<HealthcareServiceable>();
+    public void addMember(HealthcareServiceable worker){
+        members.add(worker);
+    }
+    public void removeMember(HealthcareServiceable worker){
+        members.remove(worker);
+    }
+    public void service(){
+        for(HealthcareServiceable m:members){
+            m.service();
+        }
+    }
+    public double getPrice(){
+        double total_price=0;
+        for(HealthcareServiceable m:members){
+            total_price+= m.getPrice();
+        }
+        return total_price;
+    }
+
+}
